73c8aa48632f8eefe55d688197ba098a79c0424e
Eric <ericrichardson@polymath.network>
2023-01-09 15:13:35 -0500

    Author: Eric <ericrichardson@polymath.network> Date:   Wed Jan 4 16:58:35 2023 -0500
        feat: 🎸 allow for tests to run in parallel
    
        uses new REST endpoints to allow for tests to be ran in parallel,
        resulting in a much faster time

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const restApi = process.env.REST_API_URL || 'http://localhost:3004';
+const vaultApi = process.env.VAULT_API_URL || 'http://localhost:8200';
+const vaultToken = process.env.VAULT_TOKEN || 'root';
+const vaultTransitPath = process.env.VAULT_TRANSIT_PATH || '/v1/transit';
 
 export const urls = {
   restApi,
+  vaultApi,
+  vaultToken,
+  vaultTransitPath,
 };
