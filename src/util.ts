2c1064e1265a69c70751b28f4f9247fe8ffe4c0c
Eric <ericrichardson@polymath.network>
2023-01-25 17:31:18 -0500

    style: 💄 remove unused code

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,3 @@
-import { cryptoWaitReady, mnemonicGenerate } from '@polkadot/util-crypto';
-
 export const alphabet = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
 
 export const randomNonce = (digits: number): string => {
@@ -8,27 +6,4 @@ export const randomNonce = (digits: number): string => {
     output += Math.floor(Math.random() * 10).toString(10);
   }
   return output;
-};
-
-export const signer = 'alice';
-
-export type LocalSigners = 'alice' | 'issuer' | 'investor';
-export type LocalMnemonics = Record<LocalSigners, string>;
-
-let localMnemonics: LocalMnemonics;
-
-export const getLocalMnemonics = async (): Promise<LocalMnemonics> => {
-  if (localMnemonics) return localMnemonics;
-
-  await cryptoWaitReady();
-
-  const [issuer, investor] = await Promise.all([mnemonicGenerate(), mnemonicGenerate()]);
-
-  localMnemonics = {
-    alice: '//Alice',
-    issuer,
-    investor,
-  };
-
-  return localMnemonics;
 };
