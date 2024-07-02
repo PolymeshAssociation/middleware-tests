40ff0d1a9f1581e45d816728877815bed7e7ba0e
Toms Veidemanis <toms@veidemanis.lv>
2024-07-02 21:36:52 +0300

    feat: 🎸 6.3 env support

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-VERSION='6.2.0'
+VERSION='6.3.0'
 
 # Note: for teardown the specifics don't matter to much, as long as the compose file can be properly templated
 ENV_FILE="$SCRIPT_DIR/../envs/$VERSION.env"
